0a18c2a0f85a15c7510a25539cdfc9c535b8d346
Patryk <trzodiii@gmail.com>
2024-01-29 12:59:14 +0100

    Add sign up form fill method

--- src/test/java/SignUpTest.java
+++ src/test/java/SignUpTest.java
@@ -16,12 +16,26 @@ public class SignUpTest {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        int randomNumber = (int) (Math.random()*1000);
         driver.manage().window().maximize();
         driver.get("http://www.kurs-selenium.pl/demo/");
 
         driver.findElements(By.xpath("//li[@id='li_myaccount']")).stream().
-                                                                    filter(WebElement::isDisplayed).findFirst().ifPresent(WebElement::click);
+                        filter(WebElement::isDisplayed).findFirst().ifPresent(WebElement::click);
+        String lastName = "Newman";
         driver.findElements(By.xpath("//a[text()='  Sign Up']")).get(1).click();
+        driver.findElement(By.name("firstname")).sendKeys("Joe");
+        driver.findElement(By.name("lastname")).sendKeys(lastName);
+        driver.findElement(By.name("phone")).sendKeys("123123123");
+        driver.findElement(By.name("email")).sendKeys("test" + randomNumber + "@pmail.com");
+        driver.findElement(By.name("password")).sendKeys("Test123@");
+        driver.findElement(By.name("confirmpassword")).sendKeys("Test123@");
+        driver.findElement(By.xpath("//button[text()=' Sign Up']")).click();
+        WebElement heading = driver.findElement(By.xpath("//h3[@class='RTL']"));
+        Assert.assertTrue(heading.getText().contains(lastName));
+        Assert.assertEquals(heading.getText(),"Hi, Joe Newman");
+
+
 
 
     }
